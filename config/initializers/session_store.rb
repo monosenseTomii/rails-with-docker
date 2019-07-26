ca6fd78b4d0b6175af04d39272fb167d0dd89eb6
monosenseTomii <tomii@monosense.co.jp>
2019-07-26 14:17:53 +0900

    fix: modified parameter because rails can`t connect to redis container on docker.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 Rails.application.config.session_store :redis_store, {
   servers: [
    {
-     host: ENV['REDIS_HOST'] || 'localhost',
+     host: ENV['REDIS_HOST'] || 'redis',
      port: ENV['REDIS_PORT'] || 6379,
      db: 0,
      namespace: 'session'
